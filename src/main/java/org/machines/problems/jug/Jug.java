768fb9bd6922771e40147493337accd5bf8d4d28
benja <bjwhalley1@sheffield.ac.uk>
2025-03-11 13:31:00 +0000

    chore@Jug: Added javadoc comments

--- src/main/java/org/machines/problems/jug/Jug.java
+++ src/main/java/org/machines/problems/jug/Jug.java
@@ -1,20 +1,36 @@
 package org.machines.problems.jug;
 
+/**
+ * @author benja
+ */
 public class Jug {
     private final int maxCapacity;
     private int currentCapacity;
 
+    /**
+     * @param maxCapacity
+     */
     public Jug(int maxCapacity) {
         this.maxCapacity = maxCapacity;
         this.currentCapacity = 0;
     }
+
+    /**
+     * @param maxCapacity
+     * @param currentCapacity
+     */
     private Jug(int maxCapacity, int currentCapacity) {
         this.maxCapacity = maxCapacity;
         this.currentCapacity = currentCapacity;
     }
+
+    /**
+     * @return copy of Jug instance
+     */
     public Jug copy() {
         return new Jug(maxCapacity, currentCapacity);
     }
+
     public void moveTo(Jug jug) {
         int availableSpace = jug.getMaxCapacity() - jug.getCurrentCapacity();
 
