d2ba4f30d88da5765f91452df812760aca999d2b
benja <bjwhalley1@sheffield.ac.uk>
2025-03-11 13:31:25 +0000

    chore@JugMain: Added a Third Jug

--- src/main/java/org/machines/problems/jug/JugMain.java
+++ src/main/java/org/machines/problems/jug/JugMain.java
@@ -12,22 +12,27 @@ public class JugMain {
         List<Jug> jugs = new ArrayList<>();
         jugs.add(new Jug(7));
         jugs.add(new Jug(4));
+        jugs.add(new Jug(5));
         JugState jugState = new JugState(jugs);
 
         List<Jug> goalJugs = new ArrayList<>();
         Jug goalJug1 = new Jug(7);
         Jug goalJug2 = new Jug(4);
+        Jug goalJug3 = new Jug(5);
 
         // Set the goal
         goalJug1.setCurrentCapacity(2);
         goalJug2.setCurrentCapacity(0);
+        goalJug3.setCurrentCapacity(0);
 
         goalJugs.add(goalJug1);
         goalJugs.add(goalJug2);
+        goalJugs.add(goalJug3);
 
         JugStateSpace jugStateSpaceBfs = new JugStateSpace(jugState,new JugState(goalJugs), new BFS());
         JugStateSpace jugStateSpaceDfs = new JugStateSpace(jugState,new JugState(goalJugs), new DFS());
 
         System.out.println(jugStateSpaceBfs.search());
-        System.out.println(jugStateSpaceDfs.search());    }
+        System.out.println(jugStateSpaceDfs.search());
+    }
 }
