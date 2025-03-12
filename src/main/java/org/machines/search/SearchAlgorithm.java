aa15672906aefe9f2db5e48f7c922c1d9b9f7e84
benja <bjwhalley1@sheffield.ac.uk>
2025-03-12 12:49:26 +0000

    feature@algorithms: added instance variables

--- src/main/java/org/machines/search/SearchAlgorithm.java
+++ src/main/java/org/machines/search/SearchAlgorithm.java
@@ -1,18 +1,25 @@
 package org.machines.search;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
 public abstract class SearchAlgorithm {
 
-    protected HashSet<String> visited;
+    protected HashSet<State> visited;
+    protected HashSet<State> unvisited;
+    protected HashMap<State, State> path = new HashMap<State, State>();
     protected List<State> result;
 
     public SearchAlgorithm() {
         this.visited = new HashSet<>();
+        this.unvisited = new HashSet<>();
         this.result = new ArrayList<>();
     }
 
     public abstract List<State> search(State initialState, State goalState);
+    public void addToUnvisited(State state) {
+        this.unvisited.add(state);
+    }
 }
