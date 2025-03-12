7c6d69216142ccf2a0ab10d200130052c71e34e3
benja <bjwhalley1@sheffield.ac.uk>
2025-03-12 12:50:36 +0000

    feature@State: added x & y coordinates

--- src/main/java/org/machines/search/State.java
+++ src/main/java/org/machines/search/State.java
@@ -4,11 +4,27 @@ import org.machines.graph.Node;
 
 import java.util.List;
 
-public abstract class State extends Node {
+public abstract class State extends Node<Integer> {
+
+    private final int x;
+    private final int y;
+
     public State() {
-        super();
+        this(0,0);
+    }
+
+    public State(int x, int y) {
+        super(0);
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
     }
     public abstract State copy();
     public abstract List<State> expand();
-    public abstract boolean isGoalState(State goalState, State currentState);
+    public abstract boolean isGoalState(State goalState);
 }
