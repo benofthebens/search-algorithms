a4f71086c538ea2c298d8ec7e1b838d1f0cf51e9
benja <bjwhalley1@sheffield.ac.uk>
2025-03-12 12:44:31 +0000

    update

--- src/main/java/org/machines/problems/jug/JugMain.java
+++ src/main/java/org/machines/problems/jug/JugMain.java
@@ -2,6 +2,7 @@ package org.machines.problems.jug;
 
 import org.machines.search.algorithm.BFS;
 import org.machines.search.algorithm.DFS;
+import org.machines.search.algorithm.ProgressiveDeepeningSearch;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,27 +13,30 @@ public class JugMain {
         List<Jug> jugs = new ArrayList<>();
         jugs.add(new Jug(7));
         jugs.add(new Jug(4));
-        jugs.add(new Jug(5));
+        //jugs.add(new Jug(5));
         JugState jugState = new JugState(jugs);
 
         List<Jug> goalJugs = new ArrayList<>();
         Jug goalJug1 = new Jug(7);
         Jug goalJug2 = new Jug(4);
-        Jug goalJug3 = new Jug(5);
+        //Jug goalJug3 = new Jug(5);
 
         // Set the goal
         goalJug1.setCurrentCapacity(2);
         goalJug2.setCurrentCapacity(0);
-        goalJug3.setCurrentCapacity(0);
+        //goalJug3.setCurrentCapacity(0);
 
         goalJugs.add(goalJug1);
         goalJugs.add(goalJug2);
-        goalJugs.add(goalJug3);
+        //goalJugs.add(goalJug3);
+        ProgressiveDeepeningSearch search = new ProgressiveDeepeningSearch();
+        search.setMaxDepth(20);
 
-        JugStateSpace jugStateSpaceBfs = new JugStateSpace(jugState,new JugState(goalJugs), new BFS());
+        JugStateSpace jugStateSpacePDS= new JugStateSpace(jugState,new JugState(goalJugs), search);
         JugStateSpace jugStateSpaceDfs = new JugStateSpace(jugState,new JugState(goalJugs), new DFS());
-
-        System.out.println(jugStateSpaceBfs.search());
+        JugStateSpace jugStateSpaceBfs = new JugStateSpace(jugState,new JugState(goalJugs), new BFS());
+        System.out.println(jugStateSpacePDS.search());
         System.out.println(jugStateSpaceDfs.search());
+        System.out.println(jugStateSpaceBfs.search());
     }
 }
