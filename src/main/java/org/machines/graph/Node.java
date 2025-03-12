fcb08c5c19eb0113be86ab9b9c5197754b934e7f
benja <bjwhalley1@sheffield.ac.uk>
2025-03-12 12:48:37 +0000

    feature@Node; added Generics & data instance variable

--- src/main/java/org/machines/graph/Node.java
+++ src/main/java/org/machines/graph/Node.java
@@ -5,36 +5,45 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-public class Node {
-    protected final List<Node> edges;
+public class Node<T> {
+    protected final List<Node<T>> edges;
+    protected T data;
 
-    public Node() {
-        this.edges = new ArrayList<Node>();
+    public Node(T data) {
+        this.edges = new ArrayList<Node<T>>();
+        this.data = data;
     }
 
-    protected Node(Node node) {
+    protected Node(Node<T> node) {
         this.edges = node.edges.stream()
                 .map(Node::copy)
                 .collect(Collectors.toList());
+        this.data = node.data;
     }
 
-    public Node copy() {
-        return new Node(this);
+    public Node<T> copy() {
+        return new Node<>(this);
     }
 
     @Override
     public boolean equals(Object obj) {
         if(Objects.isNull(obj)) return false;
         if(obj.getClass() != this.getClass()) return false;
-        Node node = (Node) obj;
+        Node<?> node = (Node<?>) obj;
         return this.edges.equals(node.edges);
     }
 
-    public List<Node> getEdges() {
+    public List<Node<T>> getEdges() {
         return this.edges;
     }
+    public T getData() {
+        return this.data;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
 
-    public void addEdge(Node connection) {
+    public void addEdge(Node<T> connection) {
         this.edges.add(connection);
     }
 }
